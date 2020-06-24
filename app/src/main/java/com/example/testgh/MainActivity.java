034860b87378bf7d15268b122552023a089e8ed4
joachSzo <joach.szolc@gmail.com>
2020-06-24 15:55:43 +0200

    system println

--- app/src/main/java/com/example/testgh/MainActivity.java
+++ app/src/main/java/com/example/testgh/MainActivity.java
@@ -11,5 +11,8 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        System.out.println("gałąź");
+        System.out.println("ale");
+        System.out.println("rent");
     }
 }
