63bdc984d6355efa73527db437cf829bfd9019bf
kkuzmicki <kkuzmicki@wp.pl>
2020-06-24 16:04:15 +0200

    commit

--- app/src/main/java/com/example/testgh/MainActivity.java
+++ app/src/main/java/com/example/testgh/MainActivity.java
@@ -3,13 +3,18 @@ package com.example.testgh;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity
 {
+    Button przycisk;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        przycisk = findViewById(R.id.button2);
     }
 }
